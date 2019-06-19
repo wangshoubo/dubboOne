b8a44c090ac4f730ed7f51058cce2f9e3b324bb3
wangshoubo <m13872831013@163.com>
2019-06-19 20:02:39 +0800

    初始化dubbo项目

---
+++ server/src/main/java/com/debug/mooc/dubbo/one/server/BootMoreApplication.java
@@ -0,0 +1,24 @@
+package com.debug.mooc.dubbo.one.server;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource(value = {"classpath:spring/spring-jdbc.xml","classpath:spring/spring-dubbo.xml"})
+@MapperScan(basePackages = "com.debug.mooc.dubbo.one.model.mapper")
+public class BootMoreApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BootMoreApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(BootMoreApplication.class, args);
+    }
+
+}
